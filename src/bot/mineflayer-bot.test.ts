ac70a75b3c2277846910f715230f0253cefac213
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2022-12-26 00:34:19 -0500

    clear interval in mineflayer-bot test

--- src/bot/mineflayer-bot.test.ts
+++ src/bot/mineflayer-bot.test.ts
@@ -1,5 +1,6 @@
 import EventEmitter from 'events';
 import { Bot } from 'mineflayer';
+import { HelpstartBot } from './bot.js';
 import { MineflayerBotAbstract } from './mineflayer-bot.js';
 
 const USERNAME = 'username';
@@ -7,54 +8,44 @@ const EMAIL = 'email';
 const HOST = 'host';
 const PORT = 25565;
 
-test('Initially disconnected', () => {
-  const helpstartBot = new (class extends MineflayerBotAbstract {
-    protected createMineflayerBot(): Bot {
-      return {} as Bot;
-    }
-  })(EMAIL);
+let bot: Bot;
+let helpstartBot: HelpstartBot;
 
-  expect(helpstartBot.connected).toBe(false);
-});
-
-test('Initially disconnected', () => {
-  const helpstartBot = new (class extends MineflayerBotAbstract {
-    protected createMineflayerBot(): Bot {
-      return {} as Bot;
-    }
-  })(EMAIL);
-
-  expect(helpstartBot.connected).toBe(false);
-});
-
-test('Throw if disconnect while not connected', () => {
-  const helpstartBot = new (class extends MineflayerBotAbstract {
-    protected createMineflayerBot(): Bot {
-      return {} as Bot;
-    }
-  })(EMAIL);
-
-  expect(helpstartBot.disconnect).toThrow;
-});
-
-test('Throw if disconnect while not connected', () => {
-  const helpstartBot = new (class extends MineflayerBotAbstract {
-    protected createMineflayerBot(): Bot {
-      return {} as Bot;
-    }
-  })(EMAIL);
-
-  expect(helpstartBot.disconnect).toThrow;
-});
-
-test('Connected after connection promise finishes', async () => {
-  const bot = new EventEmitter() as Bot;
-  const helpstartBot = new (class extends MineflayerBotAbstract {
+beforeEach(() => {
+  bot = jest.mocked(new EventEmitter() as Bot);
+  bot.quit = jest.fn().mockImplementation(() => {
+    bot.emit('end', 'test');
+  });
+  helpstartBot = new (class extends MineflayerBotAbstract {
     protected createMineflayerBot(): Bot {
       return bot;
     }
   })(EMAIL);
+});
 
+afterEach(() => {
+  if (helpstartBot.connected) {
+    helpstartBot.disconnect();
+  }
+});
+
+test('Initially disconnected', () => {
+  expect(helpstartBot.connected).toBe(false);
+});
+
+test('Initially disconnected', () => {
+  expect(helpstartBot.connected).toBe(false);
+});
+
+test('Throw if disconnect while not connected', () => {
+  expect(helpstartBot.disconnect).toThrow;
+});
+
+test('Throw if disconnect while not connected', () => {
+  expect(helpstartBot.disconnect).toThrow;
+});
+
+test('Connected after connection promise finishes', async () => {
   const connectPromise = helpstartBot.connect(HOST, PORT);
   bot.emit('spawn');
   await connectPromise;
@@ -63,13 +54,7 @@ test('Connected after connection promise finishes', async () => {
 });
 
 test('Name is correct', async () => {
-  const bot = jest.mocked(new EventEmitter() as Bot);
   bot.username = USERNAME;
-  const helpstartBot = new (class extends MineflayerBotAbstract {
-    protected createMineflayerBot(): Bot {
-      return bot;
-    }
-  })(EMAIL);
 
   const connectPromise = helpstartBot.connect(HOST, PORT);
   bot.emit('spawn');
@@ -79,13 +64,6 @@ test('Name is correct', async () => {
 });
 
 test('Throw if connect while connected', async () => {
-  const bot = new EventEmitter() as Bot;
-  const helpstartBot = new (class extends MineflayerBotAbstract {
-    protected createMineflayerBot(): Bot {
-      return bot;
-    }
-  })(EMAIL);
-
   const connectPromise = helpstartBot.connect(HOST, PORT);
   bot.emit('spawn');
   await connectPromise;
@@ -94,16 +72,6 @@ test('Throw if connect while connected', async () => {
 });
 
 test('Disconnecting after connection results in no longer being connected', (done) => {
-  const bot = jest.mocked(new EventEmitter() as Bot);
-  bot.quit = jest.fn().mockImplementation(() => {
-    bot.emit('end', 'test');
-  });
-  const helpstartBot = new (class extends MineflayerBotAbstract {
-    protected createMineflayerBot(): Bot {
-      return bot;
-    }
-  })(EMAIL);
-
   helpstartBot.once('end', () => {
     expect(helpstartBot.connected).toBe(false);
     done();
